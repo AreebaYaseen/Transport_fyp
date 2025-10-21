47f248b9be1b473e4d6eb50454ee4d25b6c7486e
Areeba Yaseen <areebayaseen46@gmail.com>
2025-10-21 18:10:45 +0500

    login signup screens updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:transport_fyp/screens/login/signup_screen.dart';
 import 'package:transport_fyp/screens/splash_screen.dart';
 import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
